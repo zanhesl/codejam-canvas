ff50751363eb917d5dbde637e9081e6459b29a4b
Alexander Lavrukhine <zanhesl@gmail.com>
2019-10-27 17:05:35 +0300

    feat: add fill and resize canvas features

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,44 @@
-import {image4x4, image32x32} from './images.js';
+const DEFAULT_SIZE = 512;
 
-const DEFAULT_SIZE = 256;
-
-
-const ctx = document.querySelector(`.canvas-main`)
-
+const canvas = document.querySelector(`.canvas-main`);
+const ctx = canvas.getContext('2d');
 const resizeButtons = document.querySelectorAll(`.resize-canvas button`);
+const imageSrc = document.querySelector(`.source-image`);
 
-// var request = new XMLHttpRequest();
-// request.open("GET", "../data/4x4.json", false);
-// request.send(null);
-// request.onreadystatechange = function() {
-//   if ( request.readyState === 4 && request.status === 200 ) {
-//     var my_JSON_object = JSON.parse(request.responseText);
-//     console.log(my_JSON_object);
-//   }
-// }
+function drawBackgroundHash(image) {
+  for (let i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[0].length; j++) {
+      ctx.fillStyle = `#${image[i][j]}`;
+      ctx.fillRect(j, i, 1, 1);
+    }
+  }
+};
 
-
+function drawBackgroundRGB(image){
+  for (let i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[0].length; j++) {
+      ctx.fillStyle = `rgba(${image[i][j][0]}, ${image[i][j][1]}, ${image[i][j][2]}, ${image[i][j][3]})`;
+      ctx.fillRect(j, i, 1, 1);
+    }
+  }
+};
 
 resizeButtons.forEach(function(element) {
   let resizeValue = element.innerHTML.split(`x`)[0];
   element.addEventListener(`click`, (evt) => {
-    [ctx.height, ctx.width] = [resizeValue, resizeValue];
-    ctx.style.transform = `scale(${DEFAULT_SIZE / resizeValue})`;
+    [canvas.height, canvas.width] = [resizeValue, resizeValue];
+    canvas.style.transform = `scale(${DEFAULT_SIZE / resizeValue})`;
   });
+});
+
+resizeButtons[0].addEventListener(`click`, (evt) => {
+  drawBackgroundHash(image4x4);
+});
+
+resizeButtons[1].addEventListener(`click`, (evt) => {
+  drawBackgroundRGB(image32x32);
+})
+
+resizeButtons[2].addEventListener(`click`, (evt) => {
+  ctx.drawImage(imageSrc, 0, 0, 256, 256);
 })
